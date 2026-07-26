5499500dec087d18fec77d176dec523237a9bcec
ish976 <h32111@gmail.com>
2026-07-26 12:38:19 +0900

    Add files via upload

---
+++ siwon/games/SchoolHub-v2/assets/config.js
@@ -0,0 +1,5 @@
+window.SCHOOLHUB_CONFIG = Object.freeze({
+  supabaseUrl: "https://dfjofjhlodgmevynodwg.supabase.co",
+  supabasePublishableKey: "sb_publishable_HCAsiUopabALW4SPF73poA_tiFNlnwl",
+  sections: ["year1", "year2", "year3", "year4", "year5", "year6", "secondary"]
+});
